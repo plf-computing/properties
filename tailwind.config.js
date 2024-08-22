15a50deaf5f3d80eccdb64edd88e9f9e39b2de47
plf-computing <plfcomputing@gmail.com>
2024-08-22 17:31:30 +0300

    navbar-create

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    fontFamily:{
+      openSans:["Open Sans","serif"],
+      playfair:["Playfair Display","serif"]
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+

b3413660496b9aa41e20977fae419a89ec99efef
plf-computing <plfcomputing@gmail.com>
2024-08-23 15:28:07 +0300

    adding hamburger menu and close icons on navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+ 
 }
+
+
